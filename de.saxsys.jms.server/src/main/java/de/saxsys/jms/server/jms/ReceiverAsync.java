84b2207d7919b40a9eaf2a9e56b3398cf34b87fb
Stefan Bley <stefan.bley@saxsys.de>
2013-07-24 21:00:39 +0200

    Asynchronous Receiver Message Driven Bean

---
+++ de.saxsys.jms.server/src/main/java/de/saxsys/jms/server/jms/ReceiverAsync.java
@@ -0,0 +1,19 @@
+package de.saxsys.jms.server.jms;
+
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+@MessageDriven(mappedName = "jms/exampleQueue")
+public class ReceiverAsync implements MessageListener {
+
+	@Override
+	public void onMessage(Message message) {
+		try {
+			System.out.println("I received: " + message.getBody(String.class));
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+}

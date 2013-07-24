bd8e614590f5a0185a5c27f329fbd2c4e771857e
Stefan Bley <stefan.bley@saxsys.de>
2013-07-24 20:14:33 +0200

    Sender with Connection Factory

--- de.saxsys.jms.server/src/main/java/de/saxsys/jms/server/jms/Sender.java
+++ de.saxsys.jms.server/src/main/java/de/saxsys/jms/server/jms/Sender.java
@@ -1,12 +1,36 @@
 package de.saxsys.jms.server.jms;
 
+import java.util.Date;
+
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
 
 @Stateless
 public class Sender {
 
+	@Resource
+	private ConnectionFactory connectionFactory;
+
+	@Resource(lookup = "jms/exampleQueue")
+	private Queue queue;
+
 	public String sendMessage() {
-		// TODO send message
-		return "TODO";
+		try {
+			Connection conn = connectionFactory.createConnection();
+			Session session = conn.createSession();
+			MessageProducer producer = session.createProducer(queue);
+			TextMessage message = session.createTextMessage("Hello, it's " + new Date().toString());
+			producer.send(message);
+			return message.getText();
+		} catch (JMSException e) {
+			return "ERROR";
+		}
 	}
 }

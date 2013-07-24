52592de0faa3a422dfed484484f27e397741fdb8
Stefan Bley <stefan.bley@saxsys.de>
2013-07-24 22:38:11 +0200

    Sender with JMSContext

--- de.saxsys.jms.server/src/main/java/de/saxsys/jms/server/jms/Sender.java
+++ de.saxsys.jms.server/src/main/java/de/saxsys/jms/server/jms/Sender.java
@@ -1,12 +1,25 @@
 package de.saxsys.jms.server.jms;
 
+import java.util.Date;
+
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.jms.JMSContext;
+import javax.jms.Topic;
 
 @Stateless
 public class Sender {
 
+	@Resource(lookup = "jms/exampleTopic")
+	private Topic topic;
+
+	@Inject
+	private JMSContext context;
+
 	public String sendMessage() {
-		// TODO send message
-		return "TODO";
+		String text = "Hey, it's " + new Date();
+		context.createProducer().send(topic, text);
+		return text;
 	}
 }
